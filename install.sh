24ebbad703b4e6f3f3eba7aa21dbfbe0bfc0504d
jpqa1 <78798768+jpqa1@users.noreply.github.com>
2021-03-22 09:02:08 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
-// create the virtual environment
+#create the virtual environment
 python3 -m venv venv
-// activate the virtual environment
+#activate the virtual environment
 source venv/bin/activate
+#install necessary packages for the app to run
+pip install -r requirements.txt
+#install pytest and pytest-cov (To produce coverage reports)
+pip install pytest pytest-cov
