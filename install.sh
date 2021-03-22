451f59d8d683c6a42e8e3f345bfbeb398191c89f
jpqa1 <78798768+jpqa1@users.noreply.github.com>
2021-03-22 09:03:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ python3 -m venv venv
 source venv/bin/activate
 #install necessary packages for the app to run
 pip install -r requirements.txt
-#install pytest and pytest-cov (To produce coverage reports)
-pip install pytest pytest-cov
+
